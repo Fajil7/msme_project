53ef2e5ff5b2fbc7dc39b7f0a246dbea6449386a
Fajil7 <130148146+Fajil7@users.noreply.github.com>
2024-06-10 17:33:10 +0530

    Add files via upload

---
+++ js/show.ans.js
@@ -0,0 +1,206 @@
+
+
+// start first question
+let plushEle = document.getElementById("plush");
+let answerEle = document.getElementById("answer");
+let isShowAns = false;
+
+function isShowAnsBtn() {
+    if(isShowAns === false) {
+        // Hide the answer element and change the plush text to '+'
+        answerEle.classList.add("d-none");
+        plushEle.textContent = '+';
+        isShowAns = true;
+    } else if(isShowAns === true) {
+        // Show the answer element and change the plush text to '-'
+        answerEle.classList.remove("d-none");
+        plushEle.textContent = '-';
+        isShowAns = false;
+    }
+};
+
+// end first question
+
+// start secound question
+let sePlushEl = document.getElementById("sePlush");
+let seAnswerEl = document.getElementById("seAnswer");
+let isShowSeAnswer = false;
+
+function isShowAnsBtnSe(){
+    if(isShowSeAnswer === false){
+        seAnswerEl.classList.add("d-none")
+        sePlushEl.textContent = "+";
+        isShowSeAnswer = true;
+    }else if (isShowSeAnswer === true){
+        seAnswerEl.classList.remove("d-none");
+        sePlushEl.textContent = '-';
+        isShowSeAnswer = false;
+    }
+}
+
+// end secound question
+
+// start third question
+let thPlushEl = document.getElementById("thPlush");
+let thAnswerEl = document.getElementById("thAnswer");
+let isShowThAnswer = false;
+
+function isShowAnsBtnTh(){
+    if(isShowThAnswer === false){
+        thAnswerEl.classList.add("d-none")
+        thPlushEl.textContent = "+";
+        isShowThAnswer = true;
+    }else if (isShowThAnswer === true){
+        thAnswerEl.classList.remove("d-none");
+        thPlushEl.textContent = '-';
+        isShowThAnswer = false;
+    }
+}
+
+// end third question
+
+// start four question
+let forPlushEl = document.getElementById("forPlush");
+let forAnswerEl = document.getElementById("forAnswer");
+let isShowForAnswer = false;
+
+function isShowAnsBtnFor(){
+    if(isShowForAnswer === false){
+        forAnswerEl.classList.add("d-none")
+        forPlushEl.textContent = "+";
+        isShowForAnswer = true;
+    }else if (isShowForAnswer === true){
+        forAnswerEl.classList.remove("d-none");
+        forPlushEl.textContent = '-';
+        isShowForAnswer = false;
+    }
+}
+
+// end four question
+
+// start five question
+let fivPlushEl = document.getElementById("fivPlush");
+let fivAnswerEl = document.getElementById("fivAnswer");
+let isShowFivAnswer = false;
+
+function isShowAnsBtnFiv(){
+    if(isShowFivAnswer === false){
+        fivAnswerEl.classList.add("d-none")
+        fivPlushEl.textContent = "+";
+        isShowFivAnswer = true;
+    }else if (isShowFivAnswer === true){
+        fivAnswerEl.classList.remove("d-none");
+        fivPlushEl.textContent = '-';
+        isShowFivAnswer = false;
+    }
+}
+// end five question
+
+// start six question
+let sixPlushEl = document.getElementById("sixPlush");
+let sixAnswerEl = document.getElementById("sixAnswer");
+let isShowSixAnswer = false;
+
+function isShowAnsBtnSix(){
+    if(isShowSixAnswer === false){
+        sixAnswerEl.classList.add("d-none")
+        sixPlushEl.textContent = "+";
+        isShowSixAnswer = true;
+    }else if (isShowSixAnswer === true){
+        sixAnswerEl.classList.remove("d-none");
+        sixPlushEl.textContent = '-';
+        isShowSixAnswer = false;
+    }
+}
+// end six question
+
+
+// start seven question
+let sevPlushEl = document.getElementById("sevPlush");
+let sevAnswerEl = document.getElementById("sevAnswer");
+let isShowSevAnswer = false;
+
+function isShowAnsBtnSev(){
+    if(isShowSevAnswer === false){
+        sevAnswerEl.classList.add("d-none")
+        sevPlushEl.textContent = "+";
+        isShowSevAnswer = true;
+    }else if (isShowSevAnswer === true){
+        sevAnswerEl.classList.remove("d-none");
+        sevPlushEl.textContent = '-';
+        isShowSevAnswer = false;
+    }
+}
+// end seven question
+
+// start eight question
+let eigPlushEl = document.getElementById("eigPlush");
+let eigAnswerEl = document.getElementById("eigAnswer");
+let isShowEigAnswer = false;
+
+function isShowAnsBtnEig(){
+    if(isShowEigAnswer === false){
+        eigAnswerEl.classList.add("d-none")
+        eigPlushEl.textContent = "+";
+        isShowEigAnswer = true;
+    }else if (isShowEigAnswer === true){
+        eigAnswerEl.classList.remove("d-none");
+        eigPlushEl.textContent = '-';
+        isShowEigAnswer = false;
+    }
+}
+// end eight question
+
+// start nine question
+let ninePlushEl = document.getElementById("ninePlush");
+let nineAnswerEl = document.getElementById("nineAnswer");
+let isShowNineAnswer = false;
+
+function isShowAnsBtnNine(){
+    if(isShowNineAnswer === false){
+        nineAnswerEl.classList.add("d-none")
+        ninePlushEl.textContent = "+";
+        isShowNineAnswer = true;
+    }else if (isShowNineAnswer === true){
+        nineAnswerEl.classList.remove("d-none");
+        ninePlushEl.textContent = '-';
+        isShowNineAnswer = false;
+    }
+}
+// end nine question
+
+// start ten question
+let tenPlushEl = document.getElementById("tenPlush");
+let tenAnswerEl = document.getElementById("tenAnswer");
+let isShowTenAnswer = false;
+
+function isShowAnsBtnTen(){
+    if(isShowTenAnswer === false){
+        tenAnswerEl.classList.add("d-none")
+        tenPlushEl.textContent = "+";
+        isShowTenAnswer = true;
+    }else if (isShowTenAnswer === true){
+        tenAnswerEl.classList.remove("d-none");
+        tenPlushEl.textContent = '-';
+        isShowTenAnswer = false;
+    }
+}
+// end ten question
+
+// start elevn question
+let elePlushEl = document.getElementById("elePlush");
+let eleAnswerEl = document.getElementById("eleAnswer");
+let isShowEleAnswer = false;
+
+function isShowAnsBtnEle(){
+    if(isShowEleAnswer === false){
+        eleAnswerEl.classList.add("d-none")
+        elePlushEl.textContent = "+";
+        isShowEleAnswer = true;
+    }else if (isShowEleAnswer === true){
+        eleAnswerEl.classList.remove("d-none");
+        elePlushEl.textContent = '-';
+        isShowEleAnswer = false;
+    }
+}
+// end eleven question
